c434b85d13c65a595420adb5eb818a555b6ea590
KefahWambai <kefah.ngwei@student.moringaschool.com>
2022-11-27 11:05:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+
+function saturdayFun(fun='roller-skate'){
+    return `This Saturday, I want to ${fun}`;
+}
+
+saturdayFun()
+
+function mondayWork(task = 'go to the office') {
+    return`This Monday, I will ${task}`;
+}
+
+mondayWork()
+
+function wrapAdjective (msg = 'special'){
+    console.log(`You are ${msg}`);
+}
+
+function wrapAdjective(wraps) {
+    if (wraps==="*") {
+        return (function (){
+            return `You are ${wraps}a hard worker${wraps}!`
+        })
+    }
+    else if (wraps==="||") {
+        return (function (){
+            return `You are ${wraps}a dedicated programmer${wraps}!`
+        })
+
+    }
+}   
