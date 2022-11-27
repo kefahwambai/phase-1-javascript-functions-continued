c88c7d8d95d50ae4e3f880fb4ab469502b251e62
KefahWambai <kefah.ngwei@student.moringaschool.com>
2022-11-27 12:25:26 +0300

    Update

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // code your solution here
 
 function saturdayFun(fun='roller-skate'){
-    return `This Saturday, I want to ${fun}`;
+    return `This Saturday, I want to ${fun}!`;
 }
 
 saturdayFun()
 
 function mondayWork(task = 'go to the office') {
-    return`This Monday, I will ${task}`;
+    return`This Monday, I will ${task}.`;
 }
 
 mondayWork()
